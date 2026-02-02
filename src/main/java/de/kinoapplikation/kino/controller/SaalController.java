2a40724114ed9b607da48df3a00e8943c350a0f6
n2 <114483978+n2klasse@users.noreply.github.com>
2026-02-02 11:55:16 +0100

    Hinzufügen des Saal Controllers

---
+++ src/main/java/de/kinoapplikation/kino/controller/SaalController.java
@@ -0,0 +1,53 @@
+package de.kinoapplikation.kino.controller;
+
+import de.kinoapplikation.kino.entity.Saal;
+import de.kinoapplikation.kino.service.SaalService;
+
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties.Admin;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/saal")
+public class SaalController {
+    
+    private final SaalService saalService;
+
+    public SaalController(SaalService saalService) {
+        this.saalService = saalService;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<String> addSaal(@RequestBody Saal saal) {
+        boolean addSaalPart = saalService.addSaal(saal);
+        if (addSaalPart) {
+            return ResponseEntity.ok("Saal erfolgreich hinzugefügt.");
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Fehler beim Hinzufügen des Saals.");
+        }
+    }
+
+    @GetMapping("/list")
+    public List<Saal> listeSaele() {
+        return saalService.listeSaele();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Saal> getSaalById(@PathVariable Long id) {
+        Saal saal = saalService.getSaalById(id);
+        if (saal != null) {
+            return ResponseEntity.ok(saal);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+    @DeleteMapping("/{id}/delete")
+    public ResponseEntity<String> deleteSaal(@PathVariable Long id) {
+        saalService.deleteSaal(id);
+        return ResponseEntity.ok("Saal erfolgreich gelöscht.");
+    }
+}
